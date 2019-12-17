221802a6c8068f69ff72fb23c4495347215b73bc
Tyan <960007nathan@gmail.com>
2019-12-17 16:50:18 +0800

    AP seq found (with some of them same)

--- primeAP.py
+++ primeAP.py
@@ -4,21 +4,40 @@ def find_prime_AP(a,b):
 
     # find primes in [a,b)
 
-    is_primes = [i for i in range(a,b)]
+    primes = [i for i in range(a,b)]
     for n in range(a,b):
         if n < 2:
-            is_primes.remove(n)
+            primes.remove(n)
             continue
 
         i = 2
         while i <= n**(0.5):
             if n % i == 0 or n == 1:
-                is_primes.remove(n)
+                primes.remove(n)
                 break
             i += 1
 
-    return(is_primes)
+    # find sequences
+
+    prime_AP = []
+
+    for i in primes:
+        for j in primes:
+            if j <= i:
+                continue
+
+            d = j - i
+            last_n = j + d
+            seq_ap = [i, j]
+            while last_n in primes:
+                seq_ap.append(last_n)
+                last_n += d
+
+            if len(seq_ap) > 2:
+                prime_AP.append(seq_ap)
+
+    return prime_AP
 
 if __name__ == '__main__':
-    primes = find_prime_AP(1,20)
-    print(primes)
+    primeAPs = find_prime_AP(1,40)
+    print(primeAPs)
