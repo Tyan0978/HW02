b1007d260ad97651cd353d4f1833be45b535de58
Tyan0978 <960007nathan@gmail.com>
2019-12-24 03:47:21 +0800

    top3s found

--- topThree.py
+++ topThree.py
@@ -5,7 +5,6 @@ def get_top_threes(file_in,file_out):
 
     # process file
     subjects = file_in.readline().strip('\n,').split(',')
-    subjects.insert(0, 'empty')
     students_scores = {}
     for line in file_in:
         line = line.strip()
@@ -24,9 +23,35 @@ def get_top_threes(file_in,file_out):
         students_scores[name] = scores
 
     # find top 3
+    rank_1, rank_2, rank_3 = [], [], []
+    for i in range(len(subjects)):
+        first, second, third = ['name',0], ['name',0], ['name',0]
+        for key in students_scores.keys():
+            score = int(students_scores[key][i])
+            if score > first[-1]:
+                third = second
+                second = first
+                first = [key,score]
+            elif score == first[-1]:
+                first.insert(0,key)
+            elif score > second[-1]:
+                third = second
+                second = [key,score]
+            elif score == second[-1]:
+                second.insert(0,key)
+            elif score > third[-1]:
+                third = [key,score]
+            elif score == third[-1]:
+                third.insert(0,key)
+
+        rank_1.append(','.join(first[:-1]))
+        rank_2.append(','.join(second[:-1]))
+        rank_3.append(','.join(third[:-1]))
+
+    print(rank_3)
 
     # write a new file
-    file_out.write('rank,%s,overall\n' % ','.join(subjects[1:]))
+    file_out.write('rank,%s,overall\n' % ','.join(subjects))
 
 if __name__ == '__main__':
     fin = open('sheet01.csv')
